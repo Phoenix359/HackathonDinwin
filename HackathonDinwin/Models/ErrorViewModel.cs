101d71525ff8c1abd95909d73109cd630eab5755
Dinant van Harmelen <Phoenix_359@hotmail.com>
2018-11-13 17:54:15 +0100

    Initial commit Hello World

---
+++ HackathonDinwin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HackathonDinwin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
